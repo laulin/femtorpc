5526c99cc3765551fb6126bdfe6ba4daacb91d6b
Laulin <gignops@gmail.com>
2024-12-17 15:31:51 +0100

    add ttl

--- femtorpc/serializer.py
+++ femtorpc/serializer.py
@@ -1,8 +1,10 @@
 import lz4.frame as lz4
 from base64 import urlsafe_b64encode
+from time import time
 
 from cryptography.fernet import Fernet
 import dill
+
 
 
 def get_serializer(key:bytes=None, compressed:bool=False)->dict:
@@ -11,9 +13,10 @@ def get_serializer(key:bytes=None, compressed:bool=False)->dict:
     return {"loads" : serializer.loads, "dumps": serializer.dumps}
 
 class Serializer:
-    def __init__(self, key:bytes=None, compressed:bool=False, compression_algorithm=lz4):
+    def __init__(self, key:bytes=None, compressed:bool=False, compression_algorithm=lz4, ttl:int=30):
         self._compressed = compressed
         self._compression_algorithm = compression_algorithm
+        self._ttl = ttl
         
         if key is not None:
             if not isinstance(key, bytes) or len(key) != 32:
@@ -39,7 +42,10 @@ class Serializer:
     def loads(self, serialized_data:bytes)->object:
         if self._key is not None:
             fernet = Fernet(self._key)
-            serialized_data = fernet.decrypt(serialized_data)
+            if self._ttl is not None:
+                serialized_data = fernet.decrypt_at_time(serialized_data, self._ttl, int(time()))
+            else:
+                serialized_data = fernet.decrypt(serialized_data)
 
         if self._compressed:
             serialized_data = self._compression_algorithm.decompress(serialized_data)
